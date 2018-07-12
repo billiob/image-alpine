4375c4af4787336b1b0154c207d6de8346b9cb30
Boris Faure <boris@fau.re>
2018-07-12 22:32:14 +0200

    Add Alpine 3.8 (because latest/edge is broken (at alpine) at the moment)

---
+++ 3.8/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = alpine
+IMAGE_VERSION = 3.8
+IMAGE_TITLE = Alpine Linux
+IMAGE_DESCRIPTION = Alpine Linux
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-alpine
+IMAGE_VENDOR_URL = https://www.alpinelinux.org/
+IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BASE_FLAVORS = openrc common docker-based
