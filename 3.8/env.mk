f561a7743eb7aece02422f381639ab0e46cc2e32
Boris Faure <boris@fau.re>
2018-12-18 21:45:02 +0100

    add alpine in image desc + update default kernel

--- 3.8/env.mk
+++ 3.8/env.mk
@@ -1,8 +1,8 @@
 IMAGE_NAME = alpine
 IMAGE_VERSION = 3.8
 IMAGE_TITLE = Alpine Linux
-IMAGE_DESCRIPTION = Alpine Linux
+IMAGE_DESCRIPTION = Alpine Linux 3.8
 IMAGE_SOURCE_URL = https://github.com/scaleway/image-alpine
 IMAGE_VENDOR_URL = https://www.alpinelinux.org/
-IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BOOTSCRIPT = mainline 4.14
 IMAGE_BASE_FLAVORS = openrc common docker-based
